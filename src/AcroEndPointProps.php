f94433c4927fe5ed3d689a47e0c5bfef8621b934
abhijeetnarvekar <abhijeet.narvekar@acrolinx.com>
2019-05-06 12:57:22 +0200

    DEV-20622 Adding index

---
+++ src/AcroEndPointProps.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Acrolinx\SDK;
+
+
+class AcrolinxEndPointProps
+{
+    public $clientSignature = '';
+    public $clientLocale = 'en';
+    public $serverAddress = '';
+    public $baseUrl = '';
+
+
+}
--- src/AcrolinxEndpoint.php
+++ src/AcrolinxEndpoint.php
@@ -2,21 +2,88 @@
 
 class AcrolinxEndpoint
 {
-    /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-    private $m_SampleProperty = '';
+    /**  @var string $serverAddress set the Platform URL to talk to */
+    private $serverAddress = '';
+    private $clientLocale = 'en';
+    private $props = null;
 
     /**
-     * Sample method
-     *
-     * Always create a corresponding docblock for each method, describing what it is for,
-     * this helps the phpdocumentator to properly generator the documentation
-     *
-     * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-     *
-     * @return string
+     * AcrolinxEndpoint constructor.
+     * @param $serverAddress
      */
-    public function method1($param1){
-        return "Hello World";
+    public function __construct($props)
+    {
+        $this->props = $props;
     }
+
+    /**
+     * @param $clientLocale
+     * Sets the language interface.
+     */
+    public function setClientLocale($clientLocale)
+    {
+        $this->clientLocale = $clientLocale;
+    }
+
+    /**
+     * Get server information
+     */
+    public function getServerInfo()
+    {
+
+    }
+
+    /**
+     * @return array
+     * Get common headers
+     */
+    public function getCommonHeaders()
+    {
+        return array(
+            'X-Acrolinx-Client: ' . $this->props->clientSignature,
+            'X-Acrolinx-Client-Locale: ' . $this->props->clientLocale,
+            'X-Acrolinx-Base-Url:' . $this->props->baseUrl,
+            'Content-Type: application/json'
+        );
+    }
+
+    /**
+     * @param $options
+     */
+    public function signin($options)
+    {
+
+    }
+
+    /**
+     * @param $authToken
+     */
+    public function getCapabilities($authToken)
+    {
+
+    }
+
+    /**
+     * @param $path
+     * @param $authToken
+     * @param $data
+     * POST method
+     */
+    public function post($path, $authToken, $data)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_POST, 1);
+
+        if ($data) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+
+        curl_setopt($curl, CURLOPT_URL, $this->props->serverAddress . $path);
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->getCommonHeaders());
+
+
+    }
+
 
 }
